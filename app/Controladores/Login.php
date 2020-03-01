5df83820ad4825384e60f25c55b82502d9e5e0c5
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-01 18:06:10 -0400

    controladores

---
+++ app/Controladores/Login.php
@@ -0,0 +1,55 @@
+<?php
+require_once 'Controlador.php';
+
+class Login extends Controlador{
+
+    public function __construct(){
+        $this->user = $this->modelo('MUser');
+    }
+
+    public function index(){
+        session_start();
+        if(isset($_SESSION['cod_user']) && isset($_SESSION['name']) && isset($_SESSION['type']))
+        {
+            header('location:/sisvideo/borrowing');
+        }
+        $this->vista('auth/login',[
+                                    'empresa' => 'Video club "Media"',
+                                ]);
+    }
+
+    public function comprueba(){
+        $name = '';
+        $password = '';
+        if(isset($_POST['name']))
+        {
+            $name = $_POST['name'];
+        }
+        if(isset($_POST['password']))
+        {
+            $password = $_POST['password'];
+        }
+
+        $user = $this->user->validaLogin($name, $password);
+        if($user)
+        {
+            session_start();
+            $_SESSION['cod_user'] = $user->cod_user;
+            $_SESSION['name'] = $user->name;
+            $_SESSION['type'] = $user->type;
+            
+            header('location:/sisvideo/borrowing');
+        }
+        else{
+            header('location:/sisvideo/login?name='.$name);
+        }
+    }
+
+    public function logout()
+    {
+        session_start();
+        session_destroy();
+        header('location:/sisvideo');
+    }
+
+}
