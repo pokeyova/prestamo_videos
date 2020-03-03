8f5519b4c0be55de3ff5ed3e10a9ad59c1885feb
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-03 16:43:55 -0400

    Agregando footers

---
+++ app/vistas/inc/footer.php
@@ -0,0 +1,47 @@
+
+<!-- SCRIPTS -->
+<script src="<?php echo RUTA_URL; ?>/public/assets/jquery-3.2.1.js"></script>
+<script src="<?php echo RUTA_URL; ?>/public/assets/bootstrap-3.3.7/dist/js/bootstrap.js"></script>
+
+<!-- Jquery DataTable Plugin Js -->
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/jquery.dataTables.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="<?php echo RUTA_URL;?>/public/assets/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+<script>
+
+$(document).ready(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+        lenguaje = {
+            "decimal": "",
+            "emptyTable": "No se encontraron registros",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Registros",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Registros",
+            "infoFiltered": "(Filtrado de _MAX_ total registros)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Registros",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+                }
+            };
+
+            $('[data-toggle="tooltip"]').tooltip();
+
+</script>
+
+
