381884e537d356f3533d85d09bc5c83dd0d6b0aa
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-03 16:25:12 -0400

    agregando class logo

--- app/vistas/header.php
+++ app/vistas/header.php
@@ -27,3 +27,41 @@
     <link rel="stylesheet" href="<?php echo RUTA_URL; ?>/public/css/prestamos.css">
 </head>
 <body>
+
+
+<header id="header">
+    <div class="logo">
+        <img src="<?php echo RUTA_URL.'/public/imgs/logo.jpg';?>" alt="logo">
+    </div>
+    <div class="empresa">
+        <h1>PRÉSTAMO DE VIDEOS</h1>
+    </div>
+    <div class="usuario">
+        <div class="icono">
+            <i class="far fa-user"></i>
+        </div>
+        <div class="nom_usuario">
+            <?php if($infoPersonal) echo $infoPersonal->name.' '.$infoPersonal->last_name; else echo $_SESSION['name'];?>
+        </div>
+        <div class="tipo">
+            <?php echo $_SESSION['type'];?>
+        </div>
+        <div class="estado">
+            <span>Activo</span> <i class="fa fa-circle"></i>
+        </div>
+    </div>
+</header>
+<nav class="navbar navbar-default" id="nav_bar">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- <a class="navbar-brand" href="/sisvideo/borrowing">PRESTAMOS</a> -->
+        </div>
+
+</nav>
