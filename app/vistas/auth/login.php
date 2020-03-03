f74b5fc7b989db20cb1dcfadf2ca26e735919e77
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-03 16:52:04 -0400

    carpeta auth + login

---
+++ app/vistas/auth/login.php
@@ -0,0 +1,24 @@
+<?php 
+    $old_name = '';
+    if(isset($_GET['name']))
+    {
+        $old_name = $_GET['name'];
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+
+    <link rel="stylesheet" href="<?php echo RUTA_URL; ?>/public/assets/bootstrap-3.3.7/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL; ?>/public/assets/fontawesome/css/all.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL; ?>/public/css/login.css">
+</head>
+<body>
+
+
+</body>
+</html>
