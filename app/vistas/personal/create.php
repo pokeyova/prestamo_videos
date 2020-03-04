e21c47e6a94f56921a2b6784b2cac7f855c93a60
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-04 01:50:59 -0400

    creando clases

--- app/vistas/personal/create.php
+++ app/vistas/personal/create.php
@@ -45,6 +45,45 @@
                                 <label>Carnet de identidad*:</label>
                                 <input type="number" name="ci" id="ci" class="form-control" required>
                             </div>
+                            <div class="col-md-2">
+                                <label>Expedido*:</label>
+                                <select name="issued" id="issued" class="form-control" required>
+                                    <option value="" disabled selected>Seleccione</option>
+                                    <option value="LP">LA PAZ</option>
+                                    <option value="CB">COCHABAMBA</option>
+                                    <option value="SC">SANTA CRUZ</option>
+                                    <option value="PT">POTOSI</option>
+                                    <option value="CH">CHUQUISACA</option>
+                                    <option value="TJ">TARIJA</option>
+                                    <option value="BN">BENI</option>
+                                    <option value="PD">PANDO</option>
+                                    <option value="OR">ORURO</option>
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Correo*:</label>
+                                    <input type="email" name="email" class="form-control" value="" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Usuario*:</label>
+                                    <select name="type" id="type" class="form-control" required>
+                                        <option value="" disabled selected>Seleccione</option>
+                                        <option value="ADMINISTRADOR">ADMINISTRADOR</option>
+                                        <option value="AUXILIAR">AUXILIAR</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-success">Registrar</button>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
