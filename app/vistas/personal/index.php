42b3e7fcf864c893f4cf263c9a583a34325c84a7
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-04 01:09:20 -0400

    creando index en perosnal

---
+++ app/vistas/personal/index.php
@@ -0,0 +1,88 @@
+<?php require RUTA.'/vistas/inc/header.php';?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+           <h3 class="titulo_form">PERSONAL</h3>
+        </div>
+        
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="<?php echo RUTA_URL.'/personal/create';?>" class="btn btn-sm btn-success pull-right">
+                        <span>Nuevo registro</span> <i class="fa fa-plus"></i>
+                    </a>
+                    <h2 class="titulo_panel">LISTA DEL PERSONAL</h2>
+                </div>
+                <div class="panel-body">
+                    <?php if(isset($_GET['bien'])): ?>
+                    <div class="alert alert-success">
+                        <button class="close" data-dismiss="alert">&times;</button>
+                        Registro guardado con éxito
+                    </div>
+                    <?php endif; ?>
+                    <?php if(isset($_GET['modificado'])): ?>
+                    <div class="alert alert-success">
+                        <button class="close" data-dismiss="alert">&times;</button>
+                        Modificación éxitosa
+                    </div>
+                    <?php endif; ?>
+                    <?php if(isset($_GET['eliminado'])): ?>
+                    <div class="alert alert-success">
+                        <button class="close" data-dismiss="alert">&times;</button>
+                        Registro eliminado con éxito
+                    </div>
+                    <?php endif; ?>
+                    <?php if(isset($_GET['error'])): ?>
+                    <div class="alert alert-warning">
+                        <button class="close" data-dismiss="alert">&times;</button>
+                        Algo salió mal intente nuevamente
+                    </div>
+                    <?php endif; ?>
+                    <?php if(isset($_GET['existe'])): ?>
+                    <div class="alert alert-info">
+                        <button class="close" data-dismiss="alert">&times;</button>
+                        No se puede eliminar el registro porque esta siendo utilizado
+                    </div>
+                    <?php endif; ?>
+
+                    <table class="table table-bordered table-hover data-table">
+                        <thead>
+                            <tr>
+                                <th>Código</th>
+                                <th>Usuario</th>
+                                <th>Nombre</th>
+                                <th>Correo</th>
+                                <th>Teléfono</th>
+                                <th>Tipo</th>
+                                <th>Opción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                                $contador = 1;
+                            ?>
+                            <?php foreach($datos['personals'] as $personal): ?>
+                            <tr>
+                                <td><?php echo $contador++;?></td>
+                                <td><?php echo $personal->usuario;?></td>
+                                <td><?php echo $personal->name.' '.$personal->last_name;?></td>
+                                <td><?php echo $personal->email;?></td>
+                                <td><?php echo $personal->phone;?></td>
+                                <td><?php echo $personal->tipo;?></td>
+                                <td class="btns-opciones">
+                                    <a href="#" class="evaluar"><i class="fa fa-eye" data-toggle="tooltip" data-placement="left" title="Ver"></i></a>
+
+                                    <a href="<?php echo RUTA_URL.'/personal/edit/'.$personal->cod_personal;?>" class="modificar"><i class="fa fa-edit" data-toggle="tooltip" data-placement="left" title="Modificar"></i></a>
+
+                                    <a href="" data-url="<?php echo RUTA_URL.'/personal/destroy/'.$personal->cod_personal;?>" data-toggle="modal" data-target="#modal-eliminar" class="eliminar"><i class="fa fa-trash" data-toggle="tooltip" data-placement="left" title="Eliminar"></i></a>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
