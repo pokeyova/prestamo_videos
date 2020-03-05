fb3f8396baaae9d0c4fab21130da8c4e3136dfea
pokeyova <yovanita282.gcg65@gmail.com>
2020-03-05 10:25:02 -0400

    creando clase elimiar

---
+++ app/vistas/modal/eliminar.php
@@ -0,0 +1,21 @@
+<div class="modal modal-danger fade" id="modal-eliminar">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Eliminar registro</h4>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post" id="formEliminar"></form>
+                <p id="mensajeEliminar"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-danger" id="btnEliminar">Sí, eliminar</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
